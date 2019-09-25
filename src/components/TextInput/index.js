512d3a2c553dfaae59d5c5104775f806cea7d536
Giorgio Zanni <g.zanni@hotmail.com>
2019-09-25 17:57:31 -0300

    ADD: text input

---
+++ src/components/TextInput/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import defaultTheme from '../../styles/defaultTheme'
+
+const Input = styled.input`
+  height: 50px;
+  border-radius: 30px;
+  border: 1px solid transparent;
+  background-color: ${defaultTheme.colors.white};
+  padding-left: 21px;
+  padding-right: 21px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  outline: 0;
+  font-size: ${defaultTheme.fonts.main.size};
+`
+
+const TextInput = ({
+  className,
+  disabled = false,
+  onChange,
+  placeholder = '',
+  value,
+}) => (
+  <Input
+    className={className}
+    disabled={disabled}
+    onChange={onChange}
+    placeholder={placeholder}
+    type="text"
+    value={value}
+  />
+)
+
+TextInput.propTypes = {
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+}
+
+export default TextInput
---
+++ src/stories/TextInput.stories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { storiesOf } from '@storybook/react'
+
+import TextInput from '../components/TextInput'
+
+storiesOf('TextInput', module)
+  .add('without parameters should be uncontrolled', () => <TextInput />)
+  .add('with value and callback', () => (
+    <TextInput onChange={action('change')} value="change" />
+  ))
+  .add('with only value', () => <TextInput value="change" />)
+  .add('disabled', () => <TextInput disabled={true} />)
+  .add('with placeholder', () => (
+    <TextInput placeholder="This is a placeholder" />
+  ))
