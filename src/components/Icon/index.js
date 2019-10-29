41731fc07cfe39a627d25950cbfde70ae74ea592
Adrian Parry <aparry@goplaceit.com>
2019-10-29 17:30:31 -0300

    FIX: Centered icons, removed inline style and removed color prop

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -3,23 +3,20 @@ import PropTypes from 'prop-types'
 
 const icons = require.context('./icons/', false, /\.svg$/)
 
-const Icon = ({ name, color = '#444242' }) => {
+const Icon = ({ name, ...props }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       className={`icon icon-${name}`}
+      {...props}
     >
-      <use
-        xlinkHref={icons(`./${name}.svg`) + '#Capa_1'}
-        style={{ fill: color }}
-      ></use>
+      <use xlinkHref={icons(`./${name}.svg`) + '#Capa_1'}></use>
     </svg>
   )
 }
 
 Icon.propTypes = {
-  color: PropTypes.string,
   name: PropTypes.oneOf([
     '-',
     'alert',
